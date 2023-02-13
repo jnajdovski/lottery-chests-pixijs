be28ab0218dbef66304175c88879f88f5bca60a2
Jovan <jovan.najdovski@outlook.com>
2023-02-13 18:43:47 +0100

    fix: added style

--- src/Screen.ts
+++ src/Screen.ts
@@ -4,7 +4,6 @@ import Game from './sys/Game'
 class Screen extends Game {
     protected onCreate(): void {
         this.layers.register('menu', Menu)
-
         this.layers.setActive('menu', true)
     }
 }

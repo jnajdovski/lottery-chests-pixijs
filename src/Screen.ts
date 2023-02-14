77ce70a86c550ba292584e4c4ca9dd2e795b39ee
Jovan <jovan.najdovski@outlook.com>
2023-02-14 18:51:34 +0100

    fix: register layers

--- src/Screen.ts
+++ src/Screen.ts
@@ -1,9 +1,15 @@
 import Menu from './layers/Menu'
 import Game from './sys/Game'
+import Lottery from './layers/Lottery'
+import BonusWin from './layers/BonusWin'
+import { Assets } from 'pixi.js'
 
 class Screen extends Game {
-    protected onCreate(): void {
+    protected async onCreate(): Promise<void> {
+        await Assets.load('assets/sprites.json')
         this.layers.register('menu', Menu)
+        this.layers.register('lottery', Lottery)
+        this.layers.register('bonus-win', BonusWin)
         this.layers.setActive('menu', true)
     }
 }

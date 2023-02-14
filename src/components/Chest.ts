75cd210c3923545f048db052e5e0f59835b32ad3
Jovan <jovan.najdovski@outlook.com>
2023-02-14 22:35:23 +0100

    fix: getting reward in chests

--- src/components/Chest.ts
+++ src/components/Chest.ts
@@ -1,26 +1,31 @@
 import { Texture } from "pixi.js";
-import { getWinAmount, haveWin, isBonus } from "../utils/utils";
+import { getWinData } from "../utils/utils";
 import Button from "./Button";
 
 class Chest extends Button {
+    private oppened: boolean = false
     constructor(texture: Texture, x: number, y: number, type: string) {
         super(texture, x, y, type)
     }
 
-    public open() {
-        let winData: { amount: number, bonus: boolean } = this.haveReward()
+    public open(): { haveWin: boolean, amount: number, bonus: boolean } {
+        this.oppened = true
+        let winData: { haveWin: boolean, amount: number, bonus: boolean } = getWinData()
         return winData
     }
 
     public reset() {
+        this.oppened = false
         this.disable()
     }
 
-    private haveReward(): { amount: number, bonus: boolean } {
-        return {
-            amount: getWinAmount(),
-            bonus: isBonus()
-        }
+    public activate(): void {
+        if (!this.oppened) super.activate()
+    }
+
+    public isActive(): boolean {
+        if (!super.isActive() && this.oppened) return false
+        return true
     }
 }
 export default Chest
